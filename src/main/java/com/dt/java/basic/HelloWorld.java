fa01ace69565eff53afb916f9dfff1394e73a88e
xiaoping <15000151969@163.com>
2017-04-28 01:05:03 +0800

    Add HelloWorld

--- src/main/java/com/dt/java/basic/HelloWorld.java
+++ src/main/java/com/dt/java/basic/HelloWorld.java
@@ -4,4 +4,7 @@ package com.dt.java.basic;
  * Created by MacWindows on 2017/4/28.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!!");
+    }
 }
